5d3dfc2995f574d4f6daa81ac4562a747db8012e
HolyFlou <stepanenko.nekita2020@gmail.com>
2026-09-12 12:48:23 +0300

    Додав головну функцію

--- main.c
+++ main.c
@@ -21,5 +21,20 @@ int main()
     printf("Введіть крок зміни аргументу: ");
     scanf("%lf", &delta);
     
+    N = (x2 - x1) / delta + 1;
+
+    for (int i = 1; i <= N; i++)
+    {
+        char temp[100];
+        
+        double func = (x1*x1*x1)/20 - 5*(x1*x1) + 1000;
+
+        strcat(result, "\n+----------+----------+----------+");
+        sprintf(temp, "\n|%10u|%10.2lf|%10.2lf|", i, x1, func); 
+        strcat(result, temp);
+        
+        x1 += delta; 
+    }
+
     return 0;
 }
