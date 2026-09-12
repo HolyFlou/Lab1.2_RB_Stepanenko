1b48a208f96d51625e98a06bdc1bc3743fda4674
HolyFlou <stepanenko.nekita2020@gmail.com>
2026-09-12 12:41:22 +0300

    Створив головні змінні

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <windows.h>
+
+int main()
+{
+    SetConsoleOutputCP(CP_UTF8);
+
+    double x1, x2, delta;
+    unsigned int N;
+
+    char result[4000] = "*********************************\n*      N   *     X   *   F(X)   *\n*********************************";
+
+
+    return 0;
+}
